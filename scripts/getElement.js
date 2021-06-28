ecb64db616084ed2771219729852a7b95b1b2ccd
Noora-Irene <noorairene@outlook.com>
2021-06-28 18:03:38 +0300

    camu started

---
+++ scripts/getElement.js
@@ -0,0 +1,9 @@
+const userInput = document.getElementById('numbers');
+
+const addBtn = document.getElementById('btn-add');
+const subtractBtn = document.getElementById('btn-subtract');
+const multiplyBtn = document.getElementById('btn-multiply');
+const divideBtn = document.getElementById('btn-divide');
+
+const result = document.getElementById('result');
+const calculation = document.getElementById('calculation');

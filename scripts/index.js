9da1e53b03990057160c96e89b332d2688b81705
Noora-Irene <noorairene@outlook.com>
2021-06-30 19:36:54 +0300

    Testing querySelectorAll

--- scripts/index.js
+++ scripts/index.js
@@ -1,11 +1,20 @@
 let currentResult = 0;
-const buttons = document.querySelectorAll('[type="button"]');
+const allBtn = document.querySelectorAll('[type="button"]');
+const numberBtn = document.querySelectorAll('[class="num-btn"]');
 
-for (let i = 0; i < buttons.length; i++) {
-   buttons[i]
+for (let i = 0; i < allBtn.length; i++) {
+   allBtn[i]
       .addEventListener("click", setUserInput, false);
+}
+for (let i = 0; i < numberBtn.length; i++) {
+   numberBtn[i]
+      .addEventListener("click", setResult, false);
 }
 
 function setUserInput() {
    document.getElementById("screen").innerHTML += this.value;
+}
+
+function setResult() {
+   document.getElementById("result").innerHTML += this.value;
 }

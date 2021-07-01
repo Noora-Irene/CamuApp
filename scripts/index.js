1306a5f117a7188556ca17344372efe110ea593f
Noora-Irene <noorairene@outlook.com>
2021-07-01 14:56:03 +0300

    Operators working except clear

--- scripts/index.js
+++ scripts/index.js
@@ -1,5 +1,5 @@
 const calculator = {
-   startValue: '0',
+   screenValue: '0',
    userInput1: null,
    userInput2: false,
    operator: null
@@ -14,11 +14,13 @@ buttons.addEventListener('click', (e) => {
       return;
    }
    if (target.classList.contains('operator')) {
-      console.log(target.value);
+      useOperator(target.value);
+      updScreen();
       return;
    }
    if (target.classList.contains('decimal')) {
-      console.log(target.value);
+      inputDecimal(target.value);
+      updScreen();
       return;
    }
    if (target.classList.contains('clear')) {
@@ -27,15 +29,60 @@ buttons.addEventListener('click', (e) => {
    }
    inputNumber(target.value);
    updScreen();
-})
+});
 
 function inputNumber(num) {
-   const { startValue } = calculator; // same as const startValue = calculator.startValue;
-   calculator.startValue = startValue === '0' ? num : startValue + num;
+   const { screenValue, userInput2 } = calculator;
+
+   if (userInput2 === true) {
+      calculator.screenValue = num;
+      calculator.userInput2 = false;
+   } else {
+      calculator.screenValue = screenValue === '0' ? num : screenValue + num;
+   }
+}
+
+function inputDecimal(dot) {
+   // Checks if there is already dot on screen
+   if (!calculator.screenValue.includes(dot)) {
+      calculator.screenValue += dot;
+   }
+}
+function useOperator(nextOperator) {
+   const { userInput1, screenValue, operator } = calculator
+   const i = parseFloat(screenValue);
+
+   if (operator && calculator.userInput2) {
+      calculator.operator = nextOperator;
+      return;
+   }
+
+   if (userInput1 == null && !isNaN(i)) {
+      calculator.userInput1 = i;
+   } else if (operator) {
+      const result = calculate(userInput1, i, operator);
+      calculator.screenValue = String(result);
+      calculator.userInput1 = result;
+   }
+   calculator.userInput2 = true;
+   calculator.operator = nextOperator;
+}
+
+function calculate(userInput1, userInput2, operator) {
+   if (operator === '+') {
+      return userInput1 + userInput2;
+   } else if (operator === '-') {
+      return userInput1 - userInput2;
+   } else if (operator === '*') {
+      return userInput1 * userInput2;
+   } else if (operator === '/') {
+      return userInput1 / userInput2;
+   }
+   return userInput2;
 }
 
 function updScreen() {
    const screen = document.querySelector('.screen');
-   screen.value = calculator.startValue;
+   screen.value = calculator.screenValue;
 }
 updScreen();

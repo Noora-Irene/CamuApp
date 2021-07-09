d91b3e865ffc9d22f1da9e10289659ab3c4dde7a
Noora-Irene <noorairene@outlook.com>
2021-07-09 19:51:36 +0300

    Refactored calculator script

--- scripts/index.js
+++ scripts/index.js
@@ -1,3 +1,5 @@
+const buttons = document.querySelector('.calc-btn-group');
+
 const calculator = {
    screenValue: '0',
    userInput1: null,
@@ -5,55 +7,35 @@ const calculator = {
    operator: null
 };
 
-const buttons = document.querySelector('.calc-btn-group');
 buttons.addEventListener('click', (e) => {
    const { target } = e;
+   const { value } = target;
 
    // Checks that clicked element is input type button
    if (!target.matches('[type="button"]')) {
       return;
    }
-   if (target.classList.contains('operator')) {
-      useOperator(target.value);
-      updScreen();
-      return;
+   switch (value) {
+      case '+':
+      case '-':
+      case '*':
+      case '/':
+      case '=':
+         useOperator(value);
+         break;
+      case '.':
+         inputDecimal(value);
+         break;
+      case 'clear':
+         clearAll();
+         break;
+      default:
+         if (Number.isInteger(parseFloat(value))) {
+            inputNumber(value);
+         }
    }
-   if (target.classList.contains('decimal')) {
-      inputDecimal(target.value);
-      updScreen();
-      return;
-   }
-   if (target.classList.contains('clear')) {
-      clearAll(target.value);
-      updScreen();
-      return;
-   }
-   inputNumber(target.value);
    updScreen();
 });
-
-function inputNumber(num) {
-   const { screenValue, userInput2 } = calculator;
-
-   if (userInput2 === true) {
-      calculator.screenValue = num;
-      calculator.userInput2 = false;
-   } else {
-      calculator.screenValue = screenValue === '0' ? num : screenValue + num;
-   }
-}
-
-function inputDecimal(dot) {
-   // Checks if there is already dot on screen
-   if (calculator.userInput2 === true) {
-      calculator.screenValue = '0';
-      calculator.userInput2 = false;
-      return
-   }
-   if (!calculator.screenValue.includes(dot)) {
-      calculator.screenValue += dot;
-   }
-}
 
 function useOperator(nextOperator) {
    const { userInput1, screenValue, operator } = calculator
@@ -63,7 +45,6 @@ function useOperator(nextOperator) {
       calculator.operator = nextOperator;
       return;
    }
-
    if (userInput1 === null && !isNaN(i)) {
       calculator.userInput1 = i;
    } else if (operator) {
@@ -86,6 +67,29 @@ function calculate(userInput1, userInput2, operator) {
       return userInput1 / userInput2;
    }
    return userInput2;
+}
+
+function inputDecimal(dot) {
+   // Checks if there is already dot on screen
+   if (calculator.userInput2 === true) {
+      calculator.screenValue = '0';
+      calculator.userInput2 = false;
+      return
+   }
+   if (!calculator.screenValue.includes(dot)) {
+      calculator.screenValue += dot;
+   }
+}
+
+function inputNumber(num) {
+   const { screenValue, userInput2 } = calculator;
+
+   if (userInput2 === true) {
+      calculator.screenValue = num;
+      calculator.userInput2 = false;
+   } else {
+      calculator.screenValue = screenValue === '0' ? num : screenValue + num;
+   }
 }
 
 function clearAll() {
